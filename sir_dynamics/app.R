ce6ae985dceb21e3c44e31e50d4e8dbf0c095060
Ettie Unwin <h.unwin@imperial.ac.uk>
2025-01-22 13:56:47 +0000

    Adds seirs model

--- sir_dynamics/app.R
+++ sir_dynamics/app.R
@@ -171,6 +171,97 @@ ui <- page_navbar(
                      
                      
                 )))),
+  nav_panel(title = "SEIRS",
+            page_sidebar(# App title
+              titlePanel(title =  "SEIRS (Susceptible - Exposed - Infected - Recovered - Susceptible) dynamics"),
+              
+              # Enables MathJax notation
+              withMathJax(),
+              
+              # Sidebar panel for inputs 
+              sidebar = sidebar(
+                #grid text is the slide numbers
+                tags$head(
+                  tags$style(HTML(".irs-grid-text { font-size: 14px;}
+                                   .irs--shiny .irs-min,.irs--shiny .irs-max {font-size: 14px;}
+                                   .irs--shiny .irs-from,.irs--shiny .irs-to,.irs--shiny .irs-single {font-size: 14px;}
+                                  "))
+                ),
+                sliderInput(
+                  inputId = "N",
+                  label = "Number of people (N):",
+                  min = 1,
+                  max = 1000,
+                  value = 100
+                ),
+                sliderInput(
+                  inputId = "I_init",
+                  label = "Initial number of infected people (I_init):",
+                  min = 1,
+                  max = 100,
+                  value = 1
+                ),
+                sliderInput(
+                  inputId = "beta",
+                  label = 'Infection parameter (\\( \\beta \\)):',
+                  min = 0.1,
+                  max = 10,
+                  value = 2
+                ),
+                sliderInput(
+                  inputId = "gamma",
+                  label = "Progression to infection rate (\\( \\gamma \\)):",
+                  min = 0.0001,
+                  max = 1,
+                  value = 0.5
+                ),
+                sliderInput(
+                  inputId = "sigma",
+                  label = "Recovery rate (\\( \\sigma \\)):",
+                  min = 0.0001,
+                  max = 1,
+                  value = 0.1
+                ),
+                sliderInput(
+                  inputId = "alpha",
+                  label = "Waning of immunity rate (\\( \\sigma \\)):",
+                  min = 0.0001,
+                  max = 1,
+                  value = 0.1
+                ),
+                sliderInput(
+                  inputId = "max_t",
+                  label = "Maximum time:",
+                  min = 20,
+                  max = 1000,
+                  value = 100
+                )
+              ),
+              layout_columns(
+                card(card_header("Odin model code"),
+                     tags$figure(
+                       class = "centerFigure",
+                       tags$img(
+                         src = "seir.png",
+                         width = 500,
+                         alt = "SEIRS model flow diagram"
+                       ),
+                       tags$figcaption("SEIRS model flow diagram")
+                     ),
+                     uiOutput("show_code_seirs")),
+                
+                card(card_header("Plot of dynamics"),
+                     # Output: Histogram ----
+                     plotOutput(outputId = "distPlot_seirs"),
+                     
+                     checkboxGroupInput(inputId = "trend_seirs", 
+                                        label = "Select which trends to plot:", 
+                                        choices = c("S", "E", "I", "R"),
+                                        width = "100%",
+                                        inline = TRUE)
+                     
+                     
+                )))),
   nav_spacer(),
   nav_menu(
     title = "Links",
@@ -289,6 +380,58 @@ server <- function(input, output) {
     )
   })
   
+  # Code for SEIRS plot
+  output$distPlot_seirs <- renderPlot({
+    
+    y = run_seirs_model(N = input$N, 
+                       I_init = input$I_init, 
+                       beta = input$beta,
+                       gamma = input$gamma,
+                       sigma = input$sigma,
+                       alpha = input$alpha,
+                       max_t = input$max_t)
+    
+    y_long = gather(data.frame(y), 
+                    key = variable, 
+                    value = value, 
+                    -t)
+    
+    my_colors_seirs = c("#F8766D", "#7CAE00", "#00BFC4", "#C77CFF")
+    names(my_colors_seirs) <-  rev(unique(y_long$variable))
+    
+    ## filter the data
+    filtered_data <- reactive({
+      filter(y_long, variable %in% input$trend_seirs)
+    })
+    
+    # plot the selected trends
+    ggplot(filtered_data()) +
+      geom_line(aes(t, value, col = variable), linewidth = 2) + 
+      scale_y_continuous(expand = c(0, 0), limits = c(0, input$N)) +
+      scale_x_continuous(expand = c(0, 0), limits = c(0, input$max_t*1.1)) + 
+      scale_color_manual(values = my_colors_seirs) +
+      xlab("Time") + ylab("Number of people") +
+      theme_bw() +
+      theme(legend.title=element_blank(), 
+            axis.text.x = element_text(size=16),
+            axis.text.y = element_text(size=16),
+            text = element_text(size = 16))
+  })
+  
+  # Code to show SEIRS code
+  output$show_code_seirs <- renderUI({
+    raw_lines <- readLines("seirs_model.R")
+    # insert line breaks for HTML
+    code_joined <- stringi::stri_join(raw_lines, collapse = "\n")
+    
+    tagList(
+      tags$pre(
+        tags$code(
+          HTML(code_joined)
+        )
+      )
+    )
+  })
 }
 
 shinyApp(ui = ui, server = server)
--- sir_dynamics/helper_functions.R
+++ sir_dynamics/helper_functions.R
@@ -36,3 +36,21 @@ run_seir_model = function(N, I_init, beta, sigma, gamma, max_t){
   y <- mod$run(tt)
   return (y)
 }
+
+run_seirs_model = function(N, I_init, beta, sigma, gamma, alpha, max_t){
+  # The file name of the model
+  model <- "seirs_model.R"
+  
+  # Compiles the model
+  generator <- odin::odin(model)
+  mod <- generator$new(user = list(sigma = sigma, beta = beta, gamma = gamma, 
+                                   alpha = alpha, N = N, I_init = I_init))
+  
+  
+  # Set time variable
+  tt <- seq(0, max_t, by = 0.5)
+  
+  # Runs the model
+  y <- mod$run(tt)
+  return (y)
+}
